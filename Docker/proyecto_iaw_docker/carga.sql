dbddb96d1c08323db5ebf0c9ca568e784392fa62
Alejandro314 <alejandro_roman_caballero@outlook.es>
2019-02-05 19:17:01 +0100

    cambios en la base de datos

--- Docker/proyecto_iaw_docker/carga.sql
+++ Docker/proyecto_iaw_docker/carga.sql
@@ -16,6 +16,7 @@ GRANT select,insert,update,delete ON alebuntu.* TO 'usuario'@'%' IDENTIFIED BY "
 GRANT select,insert,update,delete ON alebuntu.* TO 'usuario'@'127.0.0.1' IDENTIFIED BY "2asirtriana";
 GRANT select,insert,update,delete ON alebuntu.* TO 'usuario'@'::1' IDENTIFIED BY "2asirtriana";
 flush privileges;
+
 DROP DATABASE IF EXISTS exampleDB;
 DROP DATABASE IF EXISTS alebuntu;
 CREATE DATABASE  IF NOT EXISTS `alebuntu` /*!40100 DEFAULT CHARACTER SET latin1 */;
@@ -133,6 +134,8 @@ CREATE TABLE `sistema_operativo` (
   `nombre` varchar(30) NOT NULL,
   `version` varchar(30) NOT NULL,
   `jahr_de_lanzamiento` year(4) NOT NULL,
+  `perfil_so` varchar(300) NOT NULL,
+  `fondo_so` varchar(300) NOT NULL,
   PRIMARY KEY (`cod_so`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -256,20 +259,20 @@ INSERT INTO `alebuntu`.`manuales` (`cod_manual`, `nombre`, `fecha_publicacion`, 
 VALUES ('10', 'Construccion de android',  '2018-11-06',NULL,  '16',  'Media','https://docs.google.com/document/d/1Mf1zh3Lz9deXt-t90VzyVYOi6Q-qj5RhMSKgF9q5V40/edit?usp=sharing');
 
 
-INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento)
-VALUES ('ubuntu','16.04 Xenial Xerus',2016);
-INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento)
-VALUES ('debian','9.4 stretch',2018);
-INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento)
-VALUES ('window ','server 2008 standar',2008);
-INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento)
-VALUES ('window','server 2012 standar',2012);
-INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento)
-VALUES ('lfs','7.5',2014);
-INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento)
-VALUES ('lfs','8.3',2018);
-INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento)
-VALUES ('android','4.3 Jelly Bean',2012);
+INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento,perfil_so,fondo_so)
+VALUES ('ubuntu','16.04 Xenial Xerus',2016,'../css/iconos/ubuntu.png','../css/fondos/fondo_ubuntu.jpg');
+INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento,perfil_so,fondo_so)
+VALUES ('debian','9.4 stretch',2018,'../css/iconos/debian.png','../css/fondos/fondo_debian.png');
+INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento,perfil_so,fondo_so)
+VALUES ('window ','server 2008 standar',2008,'../css/iconos/window.jpg','../css/fondos/fondo_window_10.jpg');
+INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento,perfil_so,fondo_so)
+VALUES ('window','server 2012 standar',2012,'../css/iconos/window.jpg','../css/fondos/fondo_window_10.jpg');
+INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento,perfil_so,fondo_so)
+VALUES ('lfs','7.5',2014,'../css/iconos/lfs.png','../css/fondos/fondo_lfs.jpg');
+INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento,perfil_so,fondo_so)
+VALUES ('lfs','8.3',2018,'../css/iconos/lfs.png','../css/fondos/fondo_lfs.jpg');
+INSERT INTO sistema_operativo (nombre,version,jahr_de_lanzamiento,perfil_so,fondo_so)
+VALUES ('android','4.3 Jelly Bean',2012,'../css/iconos/android.png','../css/fondos/fondo_android.jpg');
   
 
 INSERT INTO para(cod_so,cod_manual)
